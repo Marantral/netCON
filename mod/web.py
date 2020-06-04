026aab996c188c0878cdf442d9f6200db93a81b7
Marantral <infiltration.jedi@gmail.com>
2020-06-04 16:10:58 -0400

    Update

--- mod/web.py
+++ mod/web.py
@@ -14,7 +14,7 @@ def dirb(http_hosts, hosts):
     dirbfile = hosts + "_dirb_scan.txt"
 
     print(m.bcolors.GREEN + "\n\t[*]" + m.bcolors.ENDC + "\tDoing a dirb scan on --- " + http_hosts)
-    dirb = "dirb " + http_hosts +" -o ./ScanningResults/" + dirbfile + " -S"
+    dirb = "dirb " + http_hosts +" -o ./ScanningResults/" + dirbfile
     subprocess.call(dirb, shell=True)
 
 def nikto(hosts):
@@ -55,7 +55,7 @@ def web(http, ph_http):
     input("\a\nLets Enumerate all web pages!!\a\nPRESS ENTER TO START!!")
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=None) as executor:
-        executor.map(dirb, http_hosts, hosts)
+        executor.map(dirb, http_hosts, hosts, timeout=600)
         executor.map(nikto, hosts)
 
     input("\a\n\t\tWeb Enumeration is DONE. Great Job Bro!\n\t\tPRESS ENTER TO KEEP THIS TRAIN MOVING!!")
--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-apt -y install nmap nikto dirb xsltproc python3-pip install libxml2-utils
+apt -y install nmap nikto dirb xsltproc python3-pip libxml2-utils
 pip3 install python-nmap
